76b14b6395fb731b195e79de9950770efb3e50c1
Ryan W Radtke <ryan@bigeye.com>
2022-04-27 11:41:42 -0500

    updating version reqs

--- airflow2/push_to_pypi.sh
+++ airflow2/push_to_pypi.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-
-rm -rf bigeye_airflow1.egg-info
-rm -rf build
+#!/usr/bin/bash
 rm -rf dist
-python3 setup.py sdist bdist_wheel
-twine upload dist/*
+rm -rf .egg-info
+rm -rf build
+python3 -m pip install twine setuptools wheel
+python3 setup.py bdist_wheel
+twine upload --skip-existing dist/* --verbose
+twine upload --repository codeartifact --skip-existing dist/*
+rm -rf dist
+rm -rf .egg-info
+rm -rf build
