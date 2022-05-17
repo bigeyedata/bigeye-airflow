0735e0cdbdcb5008cf640f0da035afa54ca937d4
Ryan W Radtke <ryan@bigeye.com>
2022-05-17 10:11:46 -0500

    fixing qs

--- airflow2/quickstart.sh
+++ airflow2/quickstart.sh
@@ -1,31 +1,5 @@
 #!/usr/bin/bash
 
-init=false
-run=false
-stop=false
-
-while getopts 'irsh' opt; do
-  case "$opt" in
-    i)
-      echo "Will initialize airflow."
-      init=true
-      ;;
-    r)
-      echo "Will run airflow."
-      run=true
-      ;;
-    s)
-      echo "Will stop airflow."
-      stop=true
-      ;;
-
-    ?|h)
-      echo "Usage: $(basename $0) [-i (initialize) -r (run) -s (stop)]"
-      exit 1
-      ;;
-  esac
-done
-
 # Verifying whether api cred has been set.
 if [[ ! -v BIGEYE_API_CRED_FILE ]]
 then
@@ -49,56 +23,60 @@ BIGEYE_USER=$(jq -r '.user' < "$BIGEYE_API_CRED_FILE")
 BIGEYE_PASS=$(jq -r '.password' < "$BIGEYE_API_CRED_FILE")
 BIGEYE_URL=$(jq -r '.base_url' < "$BIGEYE_API_CRED_FILE")
 
-if $init -eq true
-then
-  echo "Initializing instance."
-  deactivate
-  rm -rf venv
-  rm airflow.cfg
-  rm airflow.db
-  rm webserver_config.py
-  python3 -m venv venv
-  . venv/bin/activate
+while getopts 'irsh' opt; do
+  case "$opt" in
+    i)
+      echo "Initializing instance."
+      deactivate
+      rm -rf venv
+      rm airflow.cfg
+      rm airflow.db
+      rm webserver_config.py
+      python3 -m venv venv
+      . venv/bin/activate
 
-  pip install wheel
-  pip install --no-cache-dir -r requirements.txt
+      pip install wheel
+      pip install --no-cache-dir -r requirements.txt
 
-  # initialize the database
-  airflow db init
-  airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
-  airflow connections add 'bigeye_prod' \
-    --conn-type 'http' \
-    --conn-login "$BIGEYE_USER" \
-    --conn-password "$BIGEYE_PASS" \
-    --conn-host "$BIGEYE_URL"
-fi
+      # initialize the database
+      airflow db init
+      airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
+      airflow connections add 'bigeye_prod' \
+        --conn-type 'http' \
+        --conn-login "$BIGEYE_USER" \
+        --conn-password "$BIGEYE_PASS" \
+        --conn-host "$BIGEYE_URL"
+      ;;
+    r)
+      export AIRFLOW__CORE__KILLED_TASK_CLEANUP_TIME=604800
 
-if $run -eq true
-then
-export AIRFLOW__CORE__KILLED_TASK_CLEANUP_TIME=604800
+      . venv/bin/activate
 
-. venv/bin/activate
+      pip uninstall bigeye-airflow -y
+      pip install .
 
-pip uninstall bigeye-airflow -y
-pip install .
+      pip uninstall -r requirements.txt -y
+      pip install --no-cache-dir -r requirements.txt
 
-pip uninstall -r requirements.txt -y
-pip install --no-cache-dir -r requirements.txt
+      # start the web server, default port is 8080
+      airflow webserver --port 8080 &> ~/airflow_webserver.log &
+      echo "$!" > ~/running_airflow_webserver.pid
 
-# start the web server, default port is 8080
-airflow webserver --port 8080 &> ~/airflow_webserver.log &
-echo "$!" > ~/running_airflow_webserver.pid
+      # start the scheduler
+      # open a new terminal or else run webserver with ``-D`` option to run it as a daemon
+      airflow scheduler &> ~/airflow_scheduler.log &
+      echo "$!" > ~/running_airflow_scheduler.pid
+      ;;
+    s)
+      echo "stopping airflow."
+      pkill -9 airflow
+      rm -f  ~/running_airflow_webserver.pid
+      rm -f ~/running_airflow_scheduler.pid
+      ;;
 
-# start the scheduler
-# open a new terminal or else run webserver with ``-D`` option to run it as a daemon
-airflow scheduler &> ~/airflow_scheduler.log &
-echo "$!" > ~/running_airflow_scheduler.pid
-fi
-
-if $stop -eq true
-then
-  echo "stopping airflow."
-  pkill -9 airflow
-  rm -f  ~/running_airflow_webserver.pid
-  rm -f ~/running_airflow_scheduler.pid
-fi
+    ?|h)
+      echo "Usage: $(basename $0) [-i (initialize) -r (run) -s (stop)]"
+      exit 1
+      ;;
+  esac
+done
