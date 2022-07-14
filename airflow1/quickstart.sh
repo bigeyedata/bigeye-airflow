2ab1e0ff6e3382cf93f27ca54954fce8bc6cda6f
DoubleR <ryan@bigeye.com>
2022-07-14 13:44:07 -0500

    attempting to get quickstart running for airflow1

---
+++ airflow1/quickstart.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/bash
+
+# Verifying whether api cred has been set.
+if [[ ! -v BIGEYE_API_CRED_FILE ]]
+then
+  exit
+fi
+
+# airflow needs a home, ~/airflow is the default,
+# but you can lay foundation somewhere else if you prefer
+# (optional)
+export AIRFLOW_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "AIRFLOW_HOME: $AIRFLOW_HOME"
+
+if ! command -v jq &> /dev/null
+then
+  echo "Installing Json Query CLI"
+  sudo apt install jq
+fi
+
+
+BIGEYE_USER=$(jq -r '.user' < "$BIGEYE_API_CRED_FILE")
+BIGEYE_PASS=$(jq -r '.password' < "$BIGEYE_API_CRED_FILE")
+BIGEYE_URL=$(jq -r '.base_url' < "$BIGEYE_API_CRED_FILE")
+
+while getopts 'irsh' opt; do
+  case "$opt" in
+    i)
+      echo "Initializing instance."
+      deactivate
+      rm -rf venv
+      rm airflow.cfg
+      rm airflow.db
+      rm webserver_config.py
+      python3 -m venv venv
+      . venv/bin/activate
+
+      pip3 install wheel
+      pip3 install --no-cache-dir -r requirements.txt
+
+      # initialize the database
+      airflow db init
+      airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
+      airflow connections add 'bigeye_prod' \
+        --conn-type 'http' \
+        --conn-login "$BIGEYE_USER" \
+        --conn-password "$BIGEYE_PASS" \
+        --conn-host "$BIGEYE_URL"
+      ;;
+    r)
+      export AIRFLOW__CORE__KILLED_TASK_CLEANUP_TIME=604800
+
+      . venv/bin/activate
+
+      pip3 uninstall bigeye-airflow1 -y
+      pip3 install .
+
+      pip3 uninstall -r requirements.txt -y
+      pip3 install --no-cache-dir -r requirements.txt
+
+      # start the web server, default port is 8080
+      airflow webserver --port 8080 &> ~/airflow_webserver.log &
+      echo "$!" > ~/running_airflow_webserver.pid
+
+      # start the scheduler
+      # open a new terminal or else run webserver with ``-D`` option to run it as a daemon
+      airflow scheduler &> ~/airflow_scheduler.log &
+      echo "$!" > ~/running_airflow_scheduler.pid
+      ;;
+    s)
+      echo "stopping airflow."
+      pkill -9 airflow
+      rm -f  ~/running_airflow_webserver.pid
+      rm -f ~/running_airflow_scheduler.pid
+      ;;
+
+    ?|h)
+      echo "Usage: $(basename $0) [-i (initialize) -r (run) -s (stop)]"
+      exit 1
+      ;;
+  esac
+done
